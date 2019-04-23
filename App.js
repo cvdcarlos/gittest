244ef0f015480d5c8a9c9bca5ce6628cfbf31628
Carlos Vidal Diaz <carlos@MacBook-de-Carlos.local>
2019-04-22 22:44:51 -0400

    third commit

--- App.js
+++ App.js
@@ -21,7 +21,7 @@ export default class App extends Component<Props> {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to !</Text>
+        <Text style={styles.welcome}>hello!</Text>
         
       </View>
     );
